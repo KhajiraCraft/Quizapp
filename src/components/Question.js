9cff7ffdd3daa4537183d99d527afa9365ad57a0
KhajiraCraft <khajirachris@gmail.com>
2024-06-15 13:37:02 -0400

    This commit adds jsx content to the Question and the Answer components

---
+++ src/components/Question.js
@@ -0,0 +1,15 @@
+const Question = () => {
+
+    return(
+        <div>
+        <div className="question">
+            Who is the best players dans le ciel
+        </div>
+        <div className="answers">
+            
+        </div>
+        </div>
+    );
+};
+
+export default Question;
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,8 +1,18 @@
 import React from "react";
+import Question from "./Question"
 
 
 const Quiz =() => {
-    return <div>Quiz</div>
+    return(
+        <div className="quiz">
+            <div>
+                <div className="score">Question 1/8</div>
+            <Question />
+            </div>
+            <div className="next-button">Next Question</div>
+
+        </div>
+    );
 };
 
 export default Quiz;
